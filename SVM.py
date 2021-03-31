b9f318e9ba62ee4ee769db9dbee8d2a57ee347a6
JMGilbert <jonahmgilbert1@gmail.com>
2021-03-31 02:17:12 -0400

    Added hyperparameter tuning and variable selection

--- SVM.py
+++ SVM.py
@@ -2,38 +2,10 @@ import pandas as pd
 import numpy as np
 import math
 from sklearn.svm import SVR
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import confusion_matrix
-
-# Fitting the SVM for red wine
-
-
-reddat = pd.read_csv("./winequality-red.csv", sep = ";")
-X = reddat[reddat.columns[0:11]]
-y = reddat[reddat.columns[11]]
-
-
-
-sigma2 = y.var() * 1.5
-epsilon = sigma2/math.sqrt(len(y))
-
-clf = SVR(kernel = 'rbf', epsilon = epsilon, C = 3)
-clf.fit(X,y)
-
-y_true = y.copy()
-y_pred = clf.predict(X)
-
-print("MAD red")
-print(mean_absolute_error(y_true, y_pred))
-print("Accuracy T1 -- red")
-print(sum((y_true - y_pred).abs() < 1)/len(y))
-print("Accuracy T.5 -- red")
-print(sum((y_true - y_pred).abs() < .50)/len(y))
-print("Accuracy T.25 -- red")
-print(sum((y_true - y_pred).abs() < .25)/len(y))
-
-y_pred = np.rint(y_pred)
-print(confusion_matrix(y_true,y_pred))
+from operator import itemgetter
 
 # Fitting the SVM for white wine
 
@@ -41,25 +13,123 @@ whitedat = pd.read_csv("./winequality-white.csv", sep = ";")
 X = whitedat[whitedat.columns[0:11]]
 y = whitedat[whitedat.columns[11]]
 
+clfknn = KNeighborsClassifier(n_neighbors=3)
+clfknn.fit(X,y)
 
-sigma2 = y.var() * 1.5
+y_pred = clfknn.predict(X)
+
+sigma2 = (y - y_pred).var() * 1.5
 epsilon = sigma2/math.sqrt(len(y))
 
 clf = SVR(kernel = 'rbf', epsilon = epsilon, C = 3)
-clf.fit(X,y)
-
-y_true = y.copy()
-y_pred = clf.predict(X)
-
-print("MAD white")
-print(mean_absolute_error(y_true, y_pred))
-print("Accuracy T1 -- white")
-print(sum((y_true - y_pred).abs() < 1)/len(y))
-print("Accuracy T.5 -- white")
-print(sum((y_true - y_pred).abs() < .50)/len(y))
-print("Accuracy T.25 -- white")
-print(sum((y_true - y_pred).abs() < .25)/len(y))
 
 
-y_pred = np.rint(y_pred)
-print(confusion_matrix(y_true,y_pred))
+unchanged = 0
+droplist = []
+gamma_search = [2**x for x in range(-15,3)]
+
+wdcopy = whitedat.copy()
+train_set = wdcopy.sample(frac = 0.67, random_state = 0)
+test_set = wdcopy.drop(train_set.index)
+X_train = train_set[train_set.columns[0:11]]
+y_train = train_set[train_set.columns[11]]
+
+X_test = test_set[test_set.columns[0:11]]
+y_test = test_set[test_set.columns[11]]
+
+
+clf.set_params(gamma = 0.5)
+clf.fit(X_train,y_train)
+y_pred = clf.predict(X_test)
+
+MAD = mean_absolute_error(y_test, y_pred)
+print(MAD)
+
+while True:
+    # First, search for optimal gamma
+    wdcopy = whitedat.copy()
+    train_set = wdcopy.sample(frac = 0.67, random_state = 0)
+    test_set = wdcopy.drop(train_set.index)
+
+    X_train = train_set[train_set.columns[0:11]]
+    y_train = train_set[train_set.columns[11]]
+
+    X_test = test_set[test_set.columns[0:11]]
+    y_test = test_set[test_set.columns[11]]
+
+    X_train = X_train.drop(columns = droplist)
+    X_test = X_test.drop(columns = droplist)
+
+    GS_MAD = [None] * len(gamma_search)
+    for i in range(0,len(gamma_search)):
+        clf.set_params(gamma = gamma_search[i])
+        clf.fit(X_train,y_train)
+        y_pred = clf.predict(X_test)
+        GS_MAD[i] = mean_absolute_error(y_test, y_pred)
+
+    gamma = gamma_search[min(enumerate(GS_MAD), key=itemgetter(1))[0]]
+    print(gamma)
+    clf.set_params(gamma = gamma)
+    clf.fit(X_train,y_train)
+
+    means = X_train[X_train.columns].mean()
+    Va = [None] * len(X_train.columns)
+
+    for i in range(0,len(X_train.columns)):
+        vartemp = X_train.copy()
+        vartemp.iloc[:,0:len(vartemp.columns)] = means[0:len(vartemp.columns)]
+        vartemp.iloc[:,i] = X_train.iloc[:,i]
+        yi_pred = clf.predict(vartemp)
+        Va[i] = yi_pred.var()
+
+    Ra = Va/sum(Va)
+
+    drop_var = min(enumerate(Ra), key=itemgetter(1))[0]
+    droplist.append(X_train.columns[drop_var])
+
+    X_train = X_train.drop(columns = X_train.columns[drop_var])
+    X_test = X_test.drop(columns = X_test.columns[drop_var])
+
+    # retrain:
+
+    clf.fit(X_train,y_train)
+    y_true = y_test.copy()
+    y_pred = clf.predict(X_test)
+
+    MAD_prev = MAD
+    MAD = mean_absolute_error(y_true, y_pred)
+    print(MAD)
+    if MAD > MAD_prev:
+        droplist.pop()
+        print(droplist)
+        unchanged = unchanged + 1
+        MAD = MAD_prev
+    else:
+        unchanged = 0
+
+    if unchanged == 2 or len(droplist) == 10:
+        wdcopy = whitedat.copy()
+        train_set = wdcopy.sample(frac = 0.67, random_state = 0)
+        test_set = wdcopy.drop(train_set.index)
+
+        X_train = train_set[train_set.columns[0:11]]
+        y_train = train_set[train_set.columns[11]]
+
+        X_test = test_set[test_set.columns[0:11]]
+        y_test = test_set[test_set.columns[11]]
+
+        X_train = X_train.drop(columns = droplist)
+        X_test = X_test.drop(columns = droplist)
+        clf.fit(X_train,y_train)
+        y_true = y_test.copy()
+        y_pred = clf.predict(X_test)
+        print("MAD white")
+        print(mean_absolute_error(y_true, y_pred))
+        print("Accuracy T1 -- white")
+        print(sum((y_true - y_pred).abs() < 1)/len(y))
+        print("Accuracy T.5 -- white")
+        print(sum((y_true - y_pred).abs() < .50)/len(y))
+        print("Accuracy T.25 -- white")
+        print(sum((y_true - y_pred).abs() < .25)/len(y))
+        print(X_train.columns)
+        break
